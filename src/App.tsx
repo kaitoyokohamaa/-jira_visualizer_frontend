120dbec629d46e4a177d6e1bddb73a0bf8610518
kaitoyokohamaa <s1A121700885@toyo.jp>
2022-04-25 12:06:17 +0900

    feat: setup material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Badge from "@mui/material/Badge";
+import MailIcon from "@mui/icons-material/Mail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Badge badgeContent={4} color="primary">
+        <MailIcon color="action" />
+      </Badge>
     </div>
   );
 }
